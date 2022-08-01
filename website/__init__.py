92525c72891d4339e44486f6394806704b284c8e
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-01 23:38:54 +1200

    connecting views and auth to flask app

--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,9 @@ def create_app():
     app = Flask(__name__) #represents name for file. Initilizes the app
     app.config['SECRET_KEY'] = 'Nicholas21' #initializes secret key / config variable to secure cookies data
     
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
     return app # returns the created app function 
