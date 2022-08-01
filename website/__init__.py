047617266059c610324d642a00dd1295efe3049b
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-01 23:23:34 +1200

    comments

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+#Setting up the flask application
 
+#create a app function
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Nicholas21'
+    app = Flask(__name__) #represents name for file. Initilizes the app
+    app.config['SECRET_KEY'] = 'Nicholas21' #initializes secret key / config variable to secure cookies data
     
-    return app
+    return app # returns the created app function 
