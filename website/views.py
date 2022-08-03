38bcc7abc52c56708659415c90bc9c0379737249
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-03 21:44:57 +1200

    comments

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-
+#Blueprint allows us to have multiple views over different pages instead of confined to one page
 views = Blueprint('views', __name__)
 
+#creating route, where our users will go to
 @views.route('/')
 def home():
     return "<h1>Test</h1>"
