6951c52004e7a4a6ea52112218c393b59f505ae0
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-04 22:08:26 +1200

    creating html templates

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 #Blueprint allows us to have multiple views over different pages instead of confined to one page
 views = Blueprint('views', __name__)
 
@@ -6,4 +6,4 @@ views = Blueprint('views', __name__)
 @views.route('/')
 #this function will run everytime we activate this route
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
