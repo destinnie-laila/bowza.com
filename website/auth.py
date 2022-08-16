3182ae40069f230c93fe5338bea42c3a5a3fa5e6
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-16 22:30:53 +1200

    define http methods for login and sign up routes

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,11 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+# HTTP methods need to be defined in order to let the route know what 
+# it can and cannot do such as collect data which will change the state of the website data or database
+# 'POST' method or Retrieve data for a request 'GET' method. These are HTTP methods  
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html', text="testing")
 
@@ -10,7 +14,7 @@ def login():
 def logout():
     return '<p> Logout </p>'
 
-@auth.route('sign-up')
+@auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template('sign_up.html')
 
