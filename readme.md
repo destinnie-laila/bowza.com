f1d6730205420593d1301e143fa8fc5f75d1fd1d
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-18 15:33:54 +1200

    creating alerts and conditions for signups

--- readme.md
+++ readme.md
@@ -3,12 +3,18 @@ Readme is in progress as project goes along!
 This is a simple website development using the Flask framework with Python. 
 
 Flask methods used in this project consist of:
-    Jinja - 
     Blueprint -
-    render - template
+    render_template - 
+    request - for HTTP 'GET', 'POST' methods
+    flash - to write alert messages within the website
 
 Uses HTTP methods
 GET, POST for sign up and login
+
+Jinja - Jinja is a fast, expressive, 
+extensible templating engine. Special 
+placeholders in the template allow writing 
+code similar to Python syntax.
 
 
 The website focuses more on showcasing how we can develop a simple website with a back end and does not go into detail with css therefore the UI is basic as this more focuses on how we can use python to build a fully functional website with a back end.
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -8,9 +9,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template('login.html', text="testing")
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
@@ -18,5 +17,29 @@ def logout():
 
 @auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must contain more than 4 characters.', category="error")
+            pass
+        elif len(firstName) < 2:
+            flash('First Name must contain more than one character.', category="error")
+            pass
+        elif password1 != password2:
+            flash('Passwords must match! Please try again.', category="error")
+            pass
+        elif len(password1) < 7:
+            flash('Password must contain 7 or more characters!', category="error")
+            pass
+        else:
+            flash('Account Created!', category="success")
+            #add user tot he database
+            pass
+        
     return render_template('sign_up.html')
 
