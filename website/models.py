39fffa7dd2de9e81f3ea34eac0000be5ca95c528
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-22 22:28:50 +1200

    db created authorisation on user/new user acc

--- website/models.py
+++ website/models.py
@@ -15,7 +15,7 @@ class Note(db.Model):
     
 
 
-class User(db.Model, db.UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
