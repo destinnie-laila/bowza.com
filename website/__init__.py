0d6aee4d0f53581862b451333c482296a811698f
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-22 21:29:59 +1200

    adding sqlalchemy to init.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 #Setting up the flask application
 
 #create a app function
 def create_app():
     app = Flask(__name__) #represents name for file. Initilizes the app
     app.config['SECRET_KEY'] = 'ChaChaDu' #initializes secret key / config variable to secure cookies data
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
