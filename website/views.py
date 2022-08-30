8dd7d69f79bfa674b2cf6fd95f99e5be0ad5302d
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-31 11:54:19 +1200

    fix notes issue. spelling mistakes

--- website/views.py
+++ website/views.py
@@ -21,8 +21,8 @@ def home():
             flash('Note has no content!', category="error")
         else:
             new_note = Note(data=note, user_id=current_user.id)
-            db.sessions.add(new_note)
-            db.sessions.commit()
+            db.session.add(new_note)
+            db.session.commit()
             flash('Note has been added!', category="success")
             
     return render_template("home.html", user=current_user)
