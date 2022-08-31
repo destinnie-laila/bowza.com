622102a273f1524e7982aef712fd29789c6ec16f
destinnie-laila <68620189+destinnie-laila@users.noreply.github.com>
2022-08-31 12:17:27 +1200

    create another view for delete note

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from crypt import methods
+from typing import ValuesView
 from unicodedata import category
 from urllib import request
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 #Blueprint allows us to have multiple views over different pages instead of confined to one page
 views = Blueprint('views', __name__)
@@ -26,3 +28,14 @@ def home():
             flash('Note has been added!', category="success")
             
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.load(request.data)
+    noteID = data['note']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id = current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
